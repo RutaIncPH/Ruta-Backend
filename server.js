72c893d91ab14344d26f849b36beb03edef2d618
Jeremiah Daligues Velasco <velascojeremiahd@gmail.com>
2023-11-13 15:25:26 +0800

    Implemented sending of Origin&Destination to server

--- server.js
+++ server.js
@@ -1,9 +1,22 @@
 const express = require('express');
+const bodyparser = require('body-parser'); 
 const app = express();
 const port = 3000; // or any port you prefer
 
+app.use(bodyparser.json());
 
 app.get('/', (req, res) => {
     res.send('Welcome to Ruta-Backend Server');
   });
-  
+
+app.post('/book', (req, res) => {
+    const {origin, destination} = req.body;
+    console.log("Origin:", origin);
+    console.log("Destination:", destination);
+});
+
+app.listen(port, ()=> {
+  console.log("Server is running on port: ".port);
+})
+
+
